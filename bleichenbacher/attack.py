96d31dc0932067b905347ba0c8ba4cb99cf1d216
Nicholas Waggoner <35497241+CosmoKnot98@users.noreply.github.com>
2021-05-10 20:56:07 -0400

    Update attack.py

--- bleichenbacher/attack.py
+++ bleichenbacher/attack.py
@@ -14,6 +14,7 @@ from oracle import padding_oracle
 import helpers
 import conversions
 import oracle
+import portion as P
 
 def attack(ciphertext, n, e):
     #Implement Step 1 skip check
@@ -24,14 +25,29 @@ def attack(ciphertext, n, e):
         blind = randint(0,2^16)
     c = RSA_encrypt(blind, n, e)
     B = pow(2, 8*(len(ciphertext-2)))
-    M = [2*B, 3*B]
+    a = 2*B
+    b = (3*B) - 1
+    M = P.closed(a, b)
     i = 1
     s1 = n//(3*B)
     #Step 2: Search for more PCKS conforming messages
-    while(True):
-        #^Hate that
-
+    while(M.lower != M.upper):
+        #Step 2(a): Find s
+            
     #Step 3: Narrow solution set
 
     #Step 4: Compute the solution
-        
+    
+#Function computes c_i, returns smallest s_i where c_i is PKCS conforming
+def find_c_i(c, e, n, lower_bound, upper_bound):
+    s_i = lower_bound
+     c_i = (c * pow(s_i, e)) % n
+    #Iterates s_i and checks to see if c_i is PKCS conforming
+    while(not padding_oracle(c_i) and s_i <= upper_bound):
+        s_i += 1
+        c_i = (c * pow(s_i, e)) % n
+    if (s_i > upper_bound):
+        return 0
+    else:
+        return s_i
+    
