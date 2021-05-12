412aa4970ba6ec1c01cb695b65f77c99d721e932
CSettipane <csettipane@gmail.com>
2021-05-11 23:36:35 -0400

    added test cases

--- tests/oracle_tests.py
+++ tests/oracle_tests.py
@@ -1,10 +1,13 @@
-tests = [
+from ..bleichenbacher import oracle
 
+tests = [
+    b'', b'021234123501234', b'023045', b'000211232123304', 
+    b'0211111111111111111023'
 ]
 outputs = [
-
+    False, True, False, False, True
 ]
 
 for i in range(len(test)):
-    if tests[i]!=outputs[i]:
+    if padding_oracle(tests[i])!=outputs[i]:
         print(tests[i])
